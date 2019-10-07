6292c80a281e712ddc6790f5d493e0b6dce89abf
MadMarchello228 <mark1010_00@mail.ru>
2019-10-07 10:29:51 +0700

    Add struct RECORD

--- CURS_WORK_SIAOD/CURS_WORK_SIAOD.cpp
+++ CURS_WORK_SIAOD/CURS_WORK_SIAOD.cpp
@@ -1,5 +1,14 @@
 ﻿#include <iostream>
 using namespace std;
+
+struct record {
+	char author[12];
+	char title[32];
+	char publisher[16];
+	short int year;
+	short int num_of_page;
+};
+
 int main()
 {
 }
