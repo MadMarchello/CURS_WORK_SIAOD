5bdf2fc97e8f833fce705f0a2b2d6e4f1a86eec0
MadMarchello228 <mark1010_00@mail.ru>
2019-10-08 12:16:51 +0700

    Add new func openDatabase

--- CURS_WORK_SIAOD/CURS_WORK_SIAOD.cpp
+++ CURS_WORK_SIAOD/CURS_WORK_SIAOD.cpp
@@ -1,6 +1,12 @@
 ﻿#include <iostream>
+#include <ostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <conio.h>
 using namespace std;
-
+//1 - ошибка
+//2 - не готова функция
 struct record {
 	char author[12];
 	char title[32];
@@ -8,7 +14,32 @@ struct record {
 	short int year;
 	short int num_of_page;
 };
+void openDatabase(string filename, fstream& file) {
+	file.open(filename, ios::in | ios::binary);
+	if (!file.is_open()) {
+		throw runtime_error("File " + filename + " does not exist");
+	}
+}
 
 int main()
 {
+	fstream file;
+	string databaseName;
+	getline(cin, databaseName);
+	try {
+		openDatabase(databaseName, file);
+	}
+	catch (exception e) {
+		cout << e.what() << endl;
+		exit(1);
+	}
+	vector<record> recordArray;
+	readDatabase(file, recordArray);
+	file.close();
+	vector<record*> indexArray(recordArray.size());
+	for (int i = 0; i < indexArray.size(); i++) {
+		indexArray[i] = &recordArray[i];
+	}
+	printDatabase(indexArray);
+	return 0;
 }
